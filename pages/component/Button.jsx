b6ef56dd893b1a6a67d15db18b4b2e75d9ce3913
zDeltaDev <thehappydele@gmail.com>
2023-10-02 09:42:04 +0100

    added reusable button

---
+++ pages/component/Button.jsx
@@ -0,0 +1,7 @@
+export default function Button({ onClickHandler, children }) {
+  return (
+    <button className="w-full medium-btn default-btn" onClick={onClickHandler}>
+      {children}
+    </button>
+  );
+}
