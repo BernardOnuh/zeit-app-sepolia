3faa8deb418bfae84ed77c4c7781f3d898ed2040
midesofek <midesofek@gmail.com>
2023-11-21 14:48:48 +0100

    updated swap functions to enable ETH swap and TokenToETH swap

--- utils/swapToken.js
+++ utils/swapToken.js
@@ -1,38 +1,31 @@
 import { ethers } from "ethers";
 import ERC20ABI from "./abi/ERC20ABI";
 import ExchangeABI from "./abi/exchangeAbi";
+import { EXCHANGE_CA, TOKEN_CA } from "@/constants/constants";
 
-// 1.✅ instantiate the contract
+// 1. instantiate the contract
 // 2. approve token
 // 3. implement swap
 // add Liquidity to the contract
 // 4.
 
-const exchangeContractAddress = "0x936e5094B9aEd9C435e1D785E21de4e60B9056D6";
-const tokenAddress = "0x423E55242aE82F4a8b96dfb278B6FB47B28717D9";
-
-export default async function SwapToken(tokenAmount, ETHAmount) {
+export async function SwapETHToToken(tokenAmount, ETHAmount) {
   const provider = new ethers.providers.Web3Provider(window.ethereum);
   const signer = await provider.getSigner();
 
-  const ERC20Contract = await ethers.Contract(
-    tokenAddress,
-    ExchangeABI,
-    signer
-  );
-  const ExchangeContract = await ethers.Contract(
-    exchangeContractAddress,
+  const ERC20Contract = new ethers.Contract(TOKEN_CA, ERC20ABI, signer);
+  const ExchangeContract = new ethers.Contract(
+    EXCHANGE_CA,
     ExchangeABI,
     signer
   );
 
   //   const approve = async function (tokenAddress, amount) {
-  const approveToken = await ERC20Contract.approve(tokenAddress, tokenAmount);
+  const approveToken = await ERC20Contract.approve(TOKEN_CA, tokenAmount);
   console.log("Approving Token.....");
   await approveToken.wait();
 
   console.log("Approval Successful!");
-  //   };
 
   // ETH TO TOKEN SWAP
   console.log("Swapping ETH to Token.....");
@@ -42,6 +35,36 @@ export default async function SwapToken(tokenAmount, ETHAmount) {
   );
   await swapETHToToken.wait();
   console.log("Swap Successful!");
+}
+
+export async function SwapTokenToETH(tokenAmount, ETHAmount) {
+  const provider = new ethers.providers.Web3Provider(window.ethereum);
+  const signer = await provider.getSigner();
+
+  const ERC20Contract = new ethers.Contract(TOKEN_CA, ERC20ABI, signer);
+  const ExchangeContract = new ethers.Contract(
+    EXCHANGE_CA,
+    ExchangeABI,
+    signer
+  );
+
+  //   const approve = async function (tokenAddress, amount) {
+  const approveToken = await ERC20Contract.approve(TOKEN_CA, tokenAmount);
+  console.log("Approving Token.....");
+  await approveToken.wait();
+
+  console.log("Approval Successful!");
 
   // TOKEN TO ETH
+  console.log("Swapping ETH to Token.....");
+  const swapTokenToETH = await ExchangeContract.tokenToETHSwap(
+    ETHAmount,
+    tokenAmount
+  );
+  await swapTokenToETH.wait();
+  console.log("Swap Successful!");
+}
+
+export function NotSupported() {
+  alert("Token Pair Not Yet Supported");
 }
