096f53c8fd04c9b3fd9175e4333a082b9fbdf5ed
Bernard.O <114490070+BernardOnuh@users.noreply.github.com>
2024-02-19 21:25:43 +0100

    Update input.jsx
    Comp

--- components/reusable comp/input.jsx
+++ components/reusable comp/input.jsx
@@ -1,29 +1,67 @@
-import React from 'react'
+import React from 'react';
+import { useAccount, useBalance } from 'wagmi';
 
+function TokenComponent({ token }) {
+  const { data: tokenData, error: tokenError, isLoading: tokenIsLoading } = useBalance({
+    address: useAccount().address,
+    token: token,
+  });
 
+  if (tokenIsLoading) {
+    return <div>Loading...</div>;
+  }
 
-const Input = ({inputRef, tokenAmount, changeAmount}) => {
+  if (tokenError) {
+    return <div>Error: {tokenError.message}</div>;
+  }
+
+  // Check if tokenData is defined before accessing its properties
+  if (!tokenData) {
+    return <div>No data available</div>;
+  }
+
   return (
-    <div className="my-[8px] bg-[#F8FAFC] rounded-[8px] p-[8px] flex justify-between items-center">
-        <button className="py-[2px] px-[8px] rounded-[8px] text-[#697586] text-[14px] font-[400] font-Inter">
-            MAX
-        </button>
-        <div className="">
-            <input
-                ref={inputRef}
-                type="number"
-                name="firstTokenAmount"
-                value={tokenAmount}
-                onChange={changeAmount}
-                className="text-[#9AA4B2] h-[30px] font-Inter text-[20px] w-[57px] bg-transparent outline-none font-[500]"
-                placeholder="0.000"
-            />
-            <p className="text-[#9AA4B2] text-[14px] font-[400] font-Inter text-right">
-            $0.00
-            </p>
-        </div>
+    <div>
+      {tokenData.formatted}
     </div>
-  )
+  );
 }
 
-export default Input
+
+const Input = ({ inputRef, tokenAddress, tokenAmount, changeAmount, setMaxAmount }) => {
+  const { address } = useAccount();
+  const { data: tokenData } = useBalance({
+    address: address,
+    token: tokenAddress,
+  });
+
+  const handleMaxClick = () => {
+    if (tokenData && tokenData.formatted) {
+      changeAmount(tokenData.formatted);
+    }
+  };
+
+  return (
+    <div className="my-[8px] bg-[#F8FAFC] rounded-[8px] p-[8px] flex justify-between items-center">
+      <button onClick={handleMaxClick} className="py-[2px] px-[8px] rounded-[8px] text-[#697586] text-[14px] font-[400] font-Inter">
+        MAX
+      </button>
+      <div className="">
+        <input
+          ref={inputRef}
+          type="number"
+          name={tokenAmount}
+          value={tokenAmount}
+          onChange={changeAmount}
+          className="text-[#9AA4B2] h-[30px] font-Inter text-[20px] w-[57px] bg-transparent outline-none font-[500]"
+          placeholder="0.000"
+        />
+        <p className="text-[#9AA4B2] text-[14px] font-[400] font-Inter text-right">
+          <TokenComponent token={tokenAddress} />
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Input;
